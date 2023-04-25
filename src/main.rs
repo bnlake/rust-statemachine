6a86c7db5407a0a06d0b79b3db738366538f2fe2
Brian Lake <bnlake@gmail.com>
2023-04-25 07:35:23 -0500

    convert state machine into structs and enums

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,79 @@
-// @url https://hoverbear.org/blog/rust-state-machine-pattern/
+// @url https://play.rust-lang.org/?version=stable&mode=debug&edition=2015&gist=ee3e4df093c136ced7b394dc7ffb78e1
 
-struct BottleFillingMachine<S> {
-    shared_value: usize,
-    state: S,
-}
+use std::clone;
 
-struct Waiting {
-    waiting_time: std::time::Duration,
+#[derive(Debug, PartialEq)]
+enum State {
+    Waiting { waiting_time: u8 },
+    Filling { rate: u8 },
+    Done,
 }
-impl From<BottleFillingMachine<Done>> for BottleFillingMachine<Waiting> {
-    fn from(value: BottleFillingMachine<Done>) -> Self {
-        BottleFillingMachine::new(0)
-    }
-}
-impl BottleFillingMachine<Waiting> {
-    pub fn new(shared_value: usize) -> Self {
-        BottleFillingMachine {
-            shared_value,
-            state: Waiting {
-                waiting_time: std::time::Duration::new(0, 0),
-            },
-        }
-    }
-}
-
-struct Filling {
-    rate: usize,
-}
-impl From<BottleFillingMachine<Waiting>> for BottleFillingMachine<Filling> {
-    fn from(value: BottleFillingMachine<Waiting>) -> Self {
-        BottleFillingMachine {
-            shared_value: value.shared_value,
-            state: Filling { rate: 1 },
-        }
-    }
-}
-
-struct Done;
-impl From<BottleFillingMachine<Filling>> for BottleFillingMachine<Done> {
-    fn from(value: BottleFillingMachine<Filling>) -> Self {
-        BottleFillingMachine {
-            shared_value: value.shared_value,
-            state: Done,
-        }
-    }
-}
-
-// These enum values are not to be confused with the
-// various state structs
-enum BottleFillingMachineWrapper {
-    Waiting(BottleFillingMachine<Waiting>),
-    Filling(BottleFillingMachine<Filling>),
-    Done(BottleFillingMachine<Done>),
-}
-impl BottleFillingMachineWrapper {
-    pub fn step(mut self) -> Self {
-        match self {
-            BottleFillingMachineWrapper::Waiting(val) => {
-                BottleFillingMachineWrapper::Filling(val.into())
-            }
-            BottleFillingMachineWrapper::Filling(val) => {
-                BottleFillingMachineWrapper::Done(val.into())
-            }
-            BottleFillingMachineWrapper::Done(val) => {
-                BottleFillingMachineWrapper::Waiting(val.into())
-            }
-        }
-    }
-}
-
-struct BottleFillingMachineFactory {
-    bottle_filling_machine: BottleFillingMachineWrapper,
-}
-impl BottleFillingMachineFactory {
+impl State {
     pub fn new() -> Self {
-        BottleFillingMachineFactory {
-            bottle_filling_machine: BottleFillingMachineWrapper::Waiting(
-                BottleFillingMachine::new(0),
-            ),
+        State::Waiting { waiting_time: 0 }
+    }
+
+    pub fn next(self, event: Event) -> Self {
+        // Make a tuple and use match arms to control transitions
+        match (self, event) {
+            (State::Waiting { waiting_time }, Event::NoEvent) => State::Waiting {
+                waiting_time: waiting_time + 1,
+            },
+            (State::Waiting { .. }, Event::BottleInserted) => State::Filling { rate: 10 },
+            (State::Filling { .. }, Event::BottleFull) => State::Done,
+            (State::Done, Event::BottleEjected) => State::Waiting { waiting_time: 0 },
+            // Any other state and event are outside of the state machine boundaries
+            (s, e) => {
+                panic!("Invalid state and event: {:#?} {:#?}", s, e)
+            }
         }
     }
+
+    pub fn run(&self) {
+        match self {
+            State::Waiting { waiting_time } => {
+                println!(
+                    "We've been waiting for a bottle for {} events now",
+                    waiting_time
+                )
+            }
+            State::Filling { rate } => {
+                println!("We're filling the bottle at a rate of {}", rate)
+            }
+            State::Done => {
+                println!("Done filling")
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Event {
+    NoEvent,
+    BottleInserted,
+    BottleFull,
+    BottleEjected,
 }
 
 fn main() {
-    let mut state_machine_factory = BottleFillingMachineFactory::new();
+    let mut state = State::new();
 
-    state_machine_factory.bottle_filling_machine =
-        state_machine_factory.bottle_filling_machine.step();
+    // We'll iterate over a series of events instead
+    // of simulating events over a period of time
+    let events = [
+        Event::NoEvent,
+        Event::NoEvent,
+        Event::BottleInserted,
+        Event::BottleFull,
+        Event::BottleEjected,
+        Event::NoEvent,
+        Event::BottleFull,
+    ];
+
+    for event in events.into_iter() {
+        print!("-- Transitioning from {:?}", state);
+        state = state.next(event);
+        print!(" into {:?}", state);
+        state.run();
+    }
 }
