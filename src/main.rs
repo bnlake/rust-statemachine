126bc64a9f0529a875602b18ca831eff620ac14e
Brian Lake <bnlake@gmail.com>
2023-04-24 22:17:38 -0500

    working state machine 3 states and 2 transitions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+// @url https://hoverbear.org/blog/rust-state-machine-pattern/
+
+struct BottleFillingMachine<S> {
+    shared_value: usize,
+    state: S,
+}
+
+struct Waiting {
+    waiting_time: std::time::Duration,
+}
+impl BottleFillingMachine<Waiting> {
+    pub fn new(shared_value: usize) -> Self {
+        BottleFillingMachine {
+            shared_value,
+            state: Waiting {
+                waiting_time: std::time::Duration::new(0, 0),
+            },
+        }
+    }
+}
+
+struct Filling {
+    rate: usize,
+}
+impl From<BottleFillingMachine<Waiting>> for BottleFillingMachine<Filling> {
+    fn from(value: BottleFillingMachine<Waiting>) -> Self {
+        BottleFillingMachine {
+            shared_value: value.shared_value,
+            state: Filling { rate: 1 },
+        }
+    }
+}
+
+struct Done;
+impl From<BottleFillingMachine<Filling>> for BottleFillingMachine<Done> {
+    fn from(value: BottleFillingMachine<Filling>) -> Self {
+        BottleFillingMachine {
+            shared_value: value.shared_value,
+            state: Done,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let in_waiting = BottleFillingMachine::<Waiting>::new(0);
+    let in_filling = BottleFillingMachine::<Filling>::from(in_waiting);
 }
